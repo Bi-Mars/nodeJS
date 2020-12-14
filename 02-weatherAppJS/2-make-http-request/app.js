a7817fd86321505035bf6b6a8edb510cf95d735e
Bimarsh Sharma <bimarshsharma101@gmail.com>
2020-12-13 22:49:48 -0500

    Used Axios for HTTP request and made API call to get geocode and used geocode to get weather status

--- 02-weatherAppJS/2-make-http-request/app.js
+++ 02-weatherAppJS/2-make-http-request/app.js
@@ -1,32 +1,39 @@
 //imports
-const request = require("postman-request");
+const axios = require("axios");
 
-const url =
-  "http://api.weatherstack.com/current?access_key=84fcad3bbc7d485bae1e20c8d4f73759&query=37.8367,-122.4233&units=f";
+// Global variables
+const key_weather = "84fcad3bbc7d485bae1e20c8d4f73759";
+const key_geocode =
+  "pk.eyJ1IjoiYmltYXJzaCIsImEiOiJja2VvZmw4Y2YwMTB1Mnh0ODg3NmtjZTQ2In0.IrB2l7A6nZl4uS1mWdqEqw";
 
-//functions
-//json:true --> Parse JSON data
-request({ url: url }, (error, response) => {
-  if (error) {
-    console.log(error);
-  } else {
-    //parse JSON data
-    const data = JSON.parse(response.body);
-    // console.log(data);
-    console.log(data.current);
-  }
-});
+const url_geocode = `https://api.mapbox.com/geocoding/v5/mapbox.places/lancaster.json?access_token=${key_geocode}&limit=1`;
 
-//json:true --> Parse JSON data
-request({ url: url, json: true }, (error, response) => {
-  if (error) {
-    console.log(error);
-  } else {
-    // console.log(data);
-    console.log(
-      `${response.body.current.weather_descriptions[0]} It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike}.`
-    );
-  }
-});
+// functions
+const getWeather = (longitude, latitude) => {
+  const url_weather = `http://api.weatherstack.com/current?access_key=${key_weather}&query=${longitude},${latitude}&units=f`;
 
+  console.log(url_weather);
+
+  axios
+    .get(url_weather)
+    .then((responseData) => {
+      console.log(
+        `${responseData.data.current.weather_descriptions[0]} It is currently ${responseData.data.current.temperature} degrees out. It feels like ${responseData.data.current.feelslike}.`
+      );
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+// request to get geocode
+axios
+  .get(url_geocode)
+  .then((responseData) => {
+    const latitude = responseData.data.features[0].geometry.coordinates[0];
+    const longitude = responseData.data.features[0].geometry.coordinates[1];
+
+    getWeather(longitude, latitude);
+  })
+  .catch((error) => {});
 //exports
