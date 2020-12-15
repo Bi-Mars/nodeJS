9132be617cef4e0c582f230b8be774cabb168669
Bimarsh Sharma <bimarshsharma101@gmail.com>
2020-12-14 22:24:50 -0500

    directory structure to add CSS, JS and images, all working together

--- 04-ExpressJS-nodeJS-basedWebServer/src/app.js
+++ 04-ExpressJS-nodeJS-basedWebServer/src/app.js
@@ -8,15 +8,6 @@ const app = express();
 //serve the directories
 app.use(express.static(path.join(__dirname, "../public")));
 
-// __dirname --> contains a path to directory that current script lives in
-console.log(__dirname);
-// __filename --> contains a path to file that current file lives in
-console.log(__filename);
-
-//path --> provided by core node module and is OS compatible
-// Join path name and add .. to go 1 ldirectory up
-console.log(path.join(__dirname, "../public"));
-
 // "weather" route
 app.get("/weather", (request, response) => {
   //send JSON
