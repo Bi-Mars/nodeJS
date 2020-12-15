77038c4b2f83745a0666a2e2779cd3b91f3721bb
Bimarsh Sharma <bimarshsharma101@gmail.com>
2020-12-14 22:59:21 -0500

    restructured directory for HTML and added dynamic contents to HTML page using handler-bar (hbs) to render dynamic contents

--- 04-ExpressJS-nodeJS-basedWebServer/src/app.js
+++ 04-ExpressJS-nodeJS-basedWebServer/src/app.js
@@ -8,6 +8,38 @@ const app = express();
 //serve the directories
 app.use(express.static(path.join(__dirname, "../public")));
 
+//Tell expressJS which templating engine we are using?
+// set allows you to set a value for a given express setting
+// parameter: key and value. key for template engine --> view engine
+// express expects all of the views (handlebars template) to live in specific folder
+// root of the project > views
+app.set("view engine", "hbs");
+
+// access dynamic html via handle bar
+app.get("", (request, response) => {
+  //render specified views
+  // provide dynamic values (like Model object that contains data in Spring MVC)
+  response.render("index", {
+    title: "Weather App",
+    creater: "Bimarsh Sharma",
+  }); // index.hbs
+});
+
+// get route for about page
+app.get("/about", (request, response) => {
+  response.render("about", {
+    title: "About Me!!",
+    creater: "Bimarsh Sharma",
+  });
+});
+
+//get route for help page
+app.get("/help", (request, response) => {
+  response.render("help", {
+    help: "This page will help you out. 42",
+  });
+});
+
 // "weather" route
 app.get("/weather", (request, response) => {
   //send JSON
